b63825ed0b63675ae3e6bda524a408c5272e4d10
woogie3 <k333wook@gmail.com>
2019-12-10 12:22:37 +0900

    router_test1

--- src/index.js
+++ src/index.js
@@ -6,5 +6,3 @@ import './index.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 serviceWorker.unregister();
-export { default as Home } from './Home';
-export { default as About } from './About';
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div>
             <h2>
-                홈
+                Home
             </h2>
         </div>
     );
